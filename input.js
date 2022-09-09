90b7cc665ed4126aee62842bb3d63c3f86cc9938
MackyClap <martimac2320@gmail.com>
2022-09-09 18:42:31 -0400

    create input module

---
+++ input.js
@@ -0,0 +1,26 @@
+const net = require("net");
+const { stdin } = require("process");
+const connect = require('./client');
+
+const handleUserInput = function (key) {
+  // your code here
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 const net = require("net");
 const { stdin } = require("process");
 const connect = require('./client');
-
-const handleUserInput = function (key) {
-  // your code here
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-setupInput();
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
